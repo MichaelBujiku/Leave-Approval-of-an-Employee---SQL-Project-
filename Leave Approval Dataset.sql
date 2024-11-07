7113658715ca0bd19ff5839fbffd714e6a263446
MichaelBujiku <167851570+MichaelBujiku@users.noreply.github.com>
2024-11-07 07:47:19 +0300

    SQL Project

---
+++ Leave Approval Dataset.sql
@@ -0,0 +1,33 @@
+drop table if exists vacation_plans;
+create table vacation_plans
+(
+	id 			int primary key,
+	emp_id		int,
+	from_dt		date,
+	to_dt		date
+);
+insert into vacation_plans values(1,1, '2024-02-12', '2024-02-16');
+insert into vacation_plans values(2,2, '2024-02-20', '2024-02-29');
+insert into vacation_plans values(3,3, '2024-03-01', '2024-03-31');
+insert into vacation_plans values(4,1, '2024-04-11', '2024-04-23');
+insert into vacation_plans values(5,4, '2024-06-01', '2024-06-30');
+insert into vacation_plans values(6,3, '2024-07-05', '2024-07-15');
+insert into vacation_plans values(7,3, '2024-08-28', '2024-09-15');
+
+
+drop table if exists leave_balance;
+create table leave_balance
+(
+	emp_id			int,
+	balance			int
+);
+insert into leave_balance values (1, 12);
+insert into leave_balance values (2, 10);
+insert into leave_balance values (3, 26);
+insert into leave_balance values (4, 20);
+insert into leave_balance values (5, 14);
+
+select * from vacation_plans;
+select * from leave_balance;
+
+
